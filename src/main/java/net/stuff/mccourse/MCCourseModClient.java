6489481e6efff114035a093e22f361720fc3ed73
mengli1986 <mengli1986@gmail.com>
2024-08-11 15:13:30 -0700

    Initial commit

---
+++ src/main/java/net/stuff/mccourse/MCCourseModClient.java
@@ -0,0 +1,10 @@
+package net.stuff.mccourse;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MCCourseModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
