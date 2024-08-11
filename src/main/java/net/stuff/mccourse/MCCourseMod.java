12ed5f1c992689adaad6b53c3eb4840166536cdc
mengli1986 <mengli1986@gmail.com>
2024-08-11 15:16:47 -0700

    added a comment

--- src/main/java/net/stuff/mccourse/MCCourseMod.java
+++ src/main/java/net/stuff/mccourse/MCCourseMod.java
@@ -18,7 +18,8 @@ public class MCCourseMod implements ModInitializer {
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		// Proceed with mild caution.s
+		//very important Comment!
 
 		LOGGER.info("Hello Fabric world!");
 	}
